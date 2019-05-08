aef87e4032b09f96bd6913db3f40db4f7519cc4d
Christopher Menefee <31318316+Carcinath@users.noreply.github.com>
2019-05-08 14:09:25 -0600

    Add files via upload

---
+++ EchoServer.ex
@@ -0,0 +1,39 @@
+defmodule EchoServer do
+  require Logger
+
+  def accept(port) do
+    {:ok, socket} = :gen_tcp.listen(port,
+      [:binary, packet: :line, active: false, reuseaddr: true])
+    Logger.info "Accepting connections on port #{port}"
+    loop_acceptor(socket)
+  end
+
+  defp loop_acceptor(socket) do
+    {:ok, client} = :gen_tcp.accept(socket)
+    Task.start_link(fn -> serve(client) end)
+    loop_acceptor(socket)
+  end
+
+  defp serve(socket) do
+    socket |> read_line() |> write_line(socket)
+    :ok = :gen_tcp.close(socket)
+  end
+
+  defp read_line(socket) do
+    {:ok, data} = :gen_tcp.recv(socket, 0)
+    if File.exists?("home.html") do
+      :gen_tcp.send(socket, File.read!("home.html"))
+    else
+      :gen_tcp.send(socket, File.read!("404.html"))
+    end
+    data
+  end
+
+  defp write_line(line, socket) do
+    :gen_tcp.send(socket, line)
+  end
+
+  def main(args \\ []) do
+    accept(9999)
+  end
+end
